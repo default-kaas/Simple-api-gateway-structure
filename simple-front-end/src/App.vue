0f779147e14ff70b368364c48943439842e53ee7
default-kaas <dogen1996@live.nl>
2022-11-10 12:18:58 +0100

    Add simple front-end

---
+++ simple-front-end/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1
+      class="text-3xl text-white font-bold bg-custom-vue-green border-b-4 border-b-custom-vue-gray p-2"
+    >
+      Simple front-end
+    </h1>
+  </header>
+  <main class="p-2">Simple body</main>
+</template>
---
+++ simple-front-end/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+const colors = require("tailwindcss/colors");
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "custom-vue-green": "#42b883",
+      "custom-vue-gray": "#35495e",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      slate: colors.slate,
+      green: colors.green
+    },
+  },
+  plugins: [],
+};
